7dd4b3bd5b0e3635a30339fbb6fef5782ed11a1c
ronits2407 <ronitsonawane2007@gmail.com>
2026-01-09 18:21:29 +0530

    some problem

--- 663193D.cpp
+++ 663193D.cpp
@@ -137,7 +137,17 @@ void solve_ronit(int tc) {
         ans[i] = first;
         
         
-        
+        long long binpow(long long a, long long b, long long m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
         
         
         
